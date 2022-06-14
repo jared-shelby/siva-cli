7d20104918a9638fbfd2b6182940c60b0c9d04af
Jared Shelby <jared.shelby@yale.edu>
2022-06-14 13:40:04 -0400

    Initial setup get user input; choice between login or quit

--- bin/run.rb
+++ bin/run.rb
@@ -1,3 +1,36 @@
 require_relative '../config/environment'
 
-# binding.pry 
+# create CLI & greet user
+cli = CLI.new
+cli.greet
+
+# display options & wait for input
+puts ""
+puts "------------------------------------"
+puts "Please choose an option to continue:"
+puts "1: Login"
+puts "2: Quit"
+puts "------------------------------------"
+puts ""
+
+# store user's decision
+user_choice = nil
+
+while true
+    user_input = gets.chomp
+    puts "Your input: '#{user_input}'."
+
+    if user_input == "1"
+        user_choice = 1
+        break
+    elsif user_input == "2"
+        user_choice = 2
+        break
+    else 
+        puts "Invalid input; please try again by typing 1 or 2."
+    end
+end
+
+user_choice == 1 ? cli.login : cli.quit
+
+#binding.pry
