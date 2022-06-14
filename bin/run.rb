3291a3072861f13f1811e106c7fd6ae5f197b5a9
Jared Shelby <jared.shelby@yale.edu>
2022-06-14 13:57:34 -0400

    logic packaged & sent to cli class much cleaner now

--- bin/run.rb
+++ bin/run.rb
@@ -4,33 +4,5 @@ require_relative '../config/environment'
 cli = CLI.new
 cli.greet
 
-# display options & wait for input
-puts ""
-puts "------------------------------------"
-puts "Please choose an option to continue:"
-puts "1: Login"
-puts "2: Quit"
-puts "------------------------------------"
-puts ""
-
-# store user's decision
-user_choice = nil
-
-while true
-    user_input = gets.chomp
-    puts "Your input: '#{user_input}'."
-
-    if user_input == "1"
-        user_choice = 1
-        break
-    elsif user_input == "2"
-        user_choice = 2
-        break
-    else 
-        puts "Invalid input; please try again by typing 1 or 2."
-    end
-end
-
-user_choice == 1 ? cli.login : cli.quit
-
-#binding.pry
+# display pre-login screen
+cli.pre_login
