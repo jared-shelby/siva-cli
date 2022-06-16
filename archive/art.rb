3d2860e2fa0663bbd307bbdc3c3fb590971cb26a
Jared Shelby <jared.shelby@yale.edu>
2022-06-16 16:41:59 -0400

    remove archived files

--- archive/art.rb
+++
@@ -1,9 +0,0 @@
-# generate a banner w/ given message & buffer time
-def banner(message)
-    border = "-----------------------------------------------------------------------------"
-    self.typewriter(border, 0.01)
-    self.typewriter(message, 0.05)
-    self.typewriter(border, 0.01)
-    puts
-end
-
